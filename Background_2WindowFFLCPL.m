0ff60e524ddd5a6807fd2731c9b1f2c071cfb7fe
Barnaby Levin <bl487@cornell.edu>
2019-08-15 09:25:19 -0700

    Added modified power law and LCPL
    Modified Power Law and LCPL functions allowing an energy offset. Fairly trivial change.

--- Background_2WindowFFLCPL.m
+++ Background_2WindowFFLCPL.m
@@ -16,12 +16,12 @@ clear fitfunctionwin;
 
 
 %% Step 1: Define Energy Scale and Size of Spectrum. 
-% Define the energy scale for the linescan
 Input = Linescan_N; LinescanRaw=Linescan_N;
 Output = Input;
-String = 'LinescanSiSiO2_Power_Backsub_Test';
+String = 'LinescanSiSiO2_LCPL_Backsub';
 
-% startEn = -0.347; % Set the starting energy for the spectrum
+% % Define the energy scale for the linescan
+% startEn = 0.0; % Set the starting energy for the spectrum
 % dispersion = 0.0005; % 
 % sizeen=size(Input,2); % Calculate size of spectrum
 % sizex=size(Input,1); % Calculate size of linescan
@@ -30,7 +30,7 @@ String = 'LinescanSiSiO2_Power_Backsub_Test';
 
 Scalefactor = 1; % VERY IMPORTANT. DEFINE THE SCALE FACTOR FOR BACKGROUND SUBTRACTION
 
-% Define windows for background fitting
+% Define the sizes of the windows for background fitting
 % Windows = [Channel0+30,Channel0+40,Channel0+230,Channel0+290];
 % Windows = [Channel0+50,Channel0+85,Channel0+220,Channel0+280];
 % Windows = [Channel0+13,Channel0+16,Channel0+17,Channel0+18];
@@ -76,27 +76,29 @@ FitSpec = (Output(m,:));
 
 fitwin1=FitSpec(Windows(1):Windows(2));
 fitwin2=FitSpec(Windows(3):Windows(4));
-fitwin = cat(2,fitwin1,fitwin2); %Concatenates the values from the two windows. 
-
+% fitwin3=FitSpec(Windows(5):Windows(6));
+fitwin = cat(2,fitwin1,fitwin2);%,fitwin3); %Concatenates the values from the two windows. 
+fitEN = cat(2,EN1,EN2);%,EN3);
 
 Rawwin1=LinescanRaw(m,Windows(1):Windows(2));
 Rawwin2=LinescanRaw(m,Windows(3):Windows(4));
-Rawwin = cat(2,Rawwin1,Rawwin2); %Concatenates the values from the two windows. 
+% Rawwin3=LinescanRaw(m,Windows(5):Windows(6));
+Rawwin = cat(2,Rawwin1,Rawwin2);%,Rawwin3); %Concatenates the values from the two windows. 
 
-fitEN = cat(2,EN1,EN2);
 
-%Fit Fudge-Factor Power Law to the peaks.
-func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4);
-C1 = [max(Input(m,:))/2,0.0,3,1000]; % Initial guesses for the fit parameters
-lb=[0.0001, -0.01, 1, -10000]; % Lower bounds for the fit parameters
-ub=[Inf, 0.01, Inf, 10000]; % Upper bounds for the fit parameters 
+
+%Fit Linear Combination of 2 Power Laws to the peaks.
+func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4)*(x-C(2)).^(-C(5))+C(6);
+C1 = [max(FitSpec)/2,    0,  2.5, max(FitSpec)/2, 3.5,       1000]; % Initial guesses for the fit parameters
+lb = [             1, -0.01,    1,       0.000001,   2, -100000]; % Lower bounds for the fit parameters
+ub = [           Inf,  0.01,   10,            Inf,  10,  100000]; % Upper bounds for the fit parameters 
 FR = linspace(fitEN(1),fitEN(end));
 %figure(1);
 ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options); % Fit Curve to data.
 % plot(fitEN,fitwin,'ro',FR,func(ft,FR),'b-');
 
 % Make a measurement of the quality of the fit function within the window
-fitfunctionwin(1,:) = ft(1)*(fitEN(1,:)-ft(2)).^(-ft(3))+ft(4);
+fitfunctionwin(1,:) = ft(1)*(fitEN(1,:)-ft(2)).^(-ft(3))+ft(4)*(fitEN(1,:)-ft(2)).^(-ft(5))+ft(6);
 % SD(1,:) = abs(sqrt(Rawwin(1,:)));
 % Residual(1,:) = ((fitwin(1,:)-fitfunctionwin(1,:))./SD(1,:)).^2;
 % Estimate standard deviation from noise in low energy channels
@@ -114,7 +116,7 @@ R2 = RSquare1D(fitwin(1,:), Residual);
 RSq(1,m)=R2;
 
 % Subtract scaled fitted function from the data.  
-fitfunction(1,:) = ft(1)*(EN(1,:)-ft(2)).^(-ft(3))+ft(4);
+fitfunction(1,:) = ft(1)*(EN(1,:)-ft(2)).^(-ft(3))+ft(4)*(EN(1,:)-ft(2)).^(-ft(5))+ft(6);
 Output(m,:) = Input(m,:) - Scalefactor*fitfunction;
 Output(m,1:Windows(1)-1)=0;
 % LinescanZLPPosition = ft2(2); % Spits out position of peak.
@@ -134,18 +136,20 @@ subplot(2,2,1)
 FitSpec = (Input(Viewspec1,:));
 fitwin1=FitSpec(Windows(1):Windows(2));
 fitwin2=FitSpec(Windows(3):Windows(4));
-fitwin = cat(2,fitwin1,fitwin2); %Concatenates the values from the two windows. 
-fitEN = cat(2,EN1,EN2);
+%fitwin3=FitSpec(Windows(5):Windows(6));
+fitwin = cat(2,fitwin1,fitwin2);%,fitwin3); %Concatenates the values from the two windows. 
+fitEN = cat(2,EN1,EN2);%,EN3);
 %Fit Gaussian plus Lorentzian to the peaks.
-func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4);
-C1 = [max(Input(m,:))/2,0.0,3,1000]; % Initial guesses for the fit parameters
-lb=[0.0001, -0.01, 1, -10000]; % Lower bounds for the fit parameters
-ub=[Inf, 0.01, Inf, 10000]; % Upper bounds for the fit parameters 
-ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options); % Fit Curve to data.
+%Fit Linear Combination of 2 Power Laws to the peaks.
+func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4)*(x-C(2)).^(-C(5))+C(6);
+C1 = [max(FitSpec)/2,    0,  2.5, max(FitSpec)/2, 3.5,       0]; % Initial guesses for the fit parameters
+lb = [      0.000001, -0.01,    1,       0.000001,   1, -100000]; % Lower bounds for the fit parameters
+ub = [           Inf,  0.01,   10,            Inf,  10,  100000]; % Upper bounds for the fit parameters 
+ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options);
 hold on;
 plot(EN,Input(Viewspec1,:),'ro', FR,func(ft,FR),'b-');
-fitfunc1Pow=func(ft,FR); % Outputs the fit as an array
-fitfunc1Pow=fitfunc1Pow';
+fitfunc1LCPL=func(ft,FR); % Outputs the fit as an array
+fitfunc1LCPL=fitfunc1LCPL';
 title(['Spectrum ' num2str(Viewspec1) ':, ChiSq ' num2str(RChiSq(Viewspec1)) ', Median ' num2str(MRChiSq) ', Range ' num2str(RanRChiSq)]);
 xlim([EN(Windows(1)-PlotBuffer) EN(Windows(4)+PlotBuffer)]);
 ylim([min(fitwin) max(fitwin)]);
@@ -167,18 +171,19 @@ subplot(2,2,2)
 FitSpec = (Input(Viewspec2,:));
 fitwin1=FitSpec(Windows(1):Windows(2));
 fitwin2=FitSpec(Windows(3):Windows(4));
-fitwin = cat(2,fitwin1,fitwin2); %Concatenates the values from the two windows. 
-fitEN = cat(2,EN1,EN2);
-%Fit Gaussian plus Lorentzian to the peaks.
-func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4);
-C1 = [max(Input(m,:))/2,0.0,3,1000]; % Initial guesses for the fit parameters
-lb=[0.0001, -0.01, 1, -10000]; % Lower bounds for the fit parameters
-ub=[Inf, 0.01, Inf, 10000]; % Upper bounds for the fit parameters 
+%fitwin3=FitSpec(Windows(5):Windows(6));
+fitwin = cat(2,fitwin1,fitwin2);%,fitwin3); %Concatenates the values from the two windows. 
+fitEN = cat(2,EN1,EN2);%,EN3);
+%Fit Linear Combination of 2 Power Laws to the peaks.
+func= @(C,x)C(1)*(x-C(2)).^(-C(3))+C(4)*(x-C(2)).^(-C(5))+C(6);
+C1 = [max(FitSpec)/2,    0,  2.5, max(FitSpec)/2, 3.5,       0]; % Initial guesses for the fit parameters
+lb = [      0.000001, -0.01,    1,       0.000001,   1, -100000]; % Lower bounds for the fit parameters
+ub = [           Inf,  0.01,   10,            Inf,  10,  100000]; % Upper bounds for the fit parameters 
 ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options); % Fit Curve to data.
 hold on;
 plot(EN,Input(Viewspec2,:),'ro', FR,func(ft,FR),'b-');
-fitfunc2Pow=func(ft,FR); % Outputs the fit as an array
-fitfunc2Pow=fitfunc2Pow';
+fitfunc2LCPL=func(ft,FR); % Outputs the fit as an array
+fitfunc2LCPL=fitfunc2LCPL';
 title(['Spectrum ' num2str(Viewspec2) ': ChiSq ' num2str(RChiSq(Viewspec2))]);
 xlim([EN(Windows(1)-PlotBuffer) EN(Windows(4)+PlotBuffer)]);
 ylim([min(fitwin) max(fitwin)]);
@@ -202,6 +207,7 @@ title(['Spectrum ' num2str(Viewspec1) ': ChiSq ' num2str(RChiSq(Viewspec1)) ', M
 xlim([EN(Windows(1)-PlotBuffer) EN(Windows(4)+PlotBuffer)]);
 ylim([1.2*min(Output(Viewspec1,:)) 1.2*max(Output(Viewspec1,:))]);
 ax = gca; ax.YAxis.Exponent = 0;
+
 subplot(2,2,4)
 plot(EN,Output(Viewspec2,:),'r-');
 title(['Spectrum ' num2str(Viewspec2) ': ChiSq ' num2str(RChiSq(Viewspec2)) ', Median ChiSq ' num2str(MRChiSq)]);
@@ -209,22 +215,18 @@ xlim([EN(Windows(1)-PlotBuffer) EN(Windows(4)+PlotBuffer)]);
 ylim([1.2*min(Output(Viewspec2,:)) 1.2*max(Output(Viewspec2,:))]);
 ax = gca; ax.YAxis.Exponent = 0;
 
-num2str(MaxInd)
-
-% figure('Name','1','units','normalized','outerposition',[0 0 1 1]);
-% set(gcf, 'color', [1 1 1]);
-% 
-% subplot(2,2,1)
+% subplot(2,2,3)
 % FitSpec = (Input(MaxInd,:));
 % fitwin1=FitSpec(Windows(1):Windows(2));
 % fitwin2=FitSpec(Windows(3):Windows(4));
-% fitwin = cat(2,fitwin1,fitwin2); %Concatenates the values from the two windows. 
-% fitEN = cat(2,EN1,EN2);
+% %fitwin3=FitSpec(Windows(5):Windows(6));
+% fitwin = cat(2,fitwin1,fitwin2);%,fitwin3); %Concatenates the values from the two windows. 
+% fitEN = cat(2,EN1,EN2);%,EN3);
 % %Fit Gaussian plus Lorentzian to the peaks.
-% func= @(C,x)C(1)*x.^(-C(2))+C(3)*x+C(4);
-% C1 = [max(FitSpec)/2,3,max(FitSpec)/20,0]; % Initial guesses for the fit parameters
-% lb=[0.00001, 1, -Inf, -10000]; % Lower bounds for the fit parameters
-% ub=[Inf, Inf, Inf, 10000]; % Upper bounds for the fit parameters ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options);
+% func= @(C,x)C(1)*x.^(-C(2))+C(3)*x.^(-C(4))+C(5);
+% C1 = [max(FitSpec)/2,2.5,max(FitSpec)/2,3.5,0]; % Initial guesses for the fit parameters
+% lb=[0.000001, 1, 0.000001, 1, -100000]; % Lower bounds for the fit parameters
+% ub=[Inf, 10, Inf, 10, 100000]; % Upper bounds for the fit parameters 
 % ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options); % Fit Curve to data.
 % hold on;
 % plot(EN,Input(MaxInd,:),'ro', FR,func(ft,FR),'b-');
@@ -245,17 +247,18 @@ num2str(MaxInd)
 % box on;
 % hold off;
 % 
-% subplot(2,2,2)
+% subplot(2,2,4)
 % FitSpec = (Input(MinInd,:));
 % fitwin1=FitSpec(Windows(1):Windows(2));
 % fitwin2=FitSpec(Windows(3):Windows(4));
-% fitwin = cat(2,fitwin1,fitwin2); %Concatenates the values from the two windows. 
-% fitEN = cat(2,EN1,EN2);
+% %fitwin3=FitSpec(Windows(5):Windows(6));
+% fitwin = cat(2,fitwin1,fitwin2);%,fitwin3); %Concatenates the values from the two windows. 
+% fitEN = cat(2,EN1,EN2);%,EN3);
 % %Fit Gaussian plus Lorentzian to the peaks.
-% func= @(C,x)C(1)*x.^(-C(2))+C(3)*x+C(4);
-% C1 = [max(FitSpec)/2,3,max(FitSpec)/20,0]; % Initial guesses for the fit parameters
-% lb=[0.00001, 1, -Inf, -10000]; % Lower bounds for the fit parameters
-% ub=[Inf, Inf, Inf, 10000]; % Upper bounds for the fit parameters ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options);
+% func= @(C,x)C(1)*x.^(-C(2))+C(3)*x.^(-C(4))+C(5);
+% C1 = [max(FitSpec)/2,2.5,max(FitSpec)/2,3.5,0]; % Initial guesses for the fit parameters
+% lb=[0.000001, 1, 0.000001, 1, -100000]; % Lower bounds for the fit parameters
+% ub=[Inf, 10, Inf, 10, 1000]; % Upper bounds for the fit parameters 
 % ft = lsqcurvefit(func,C1,fitEN,fitwin, lb, ub, options); % Fit Curve to data.
 % hold on;
 % plot(EN,Input(MinInd,:),'ro', FR,func(ft,FR),'b-');
@@ -275,6 +278,9 @@ num2str(MaxInd)
 % plot([x4 x4],y4, 'g-') 
 % box on;
 % hold off;
+
+% figure('Name','1','units','normalized','outerposition',[0 0 1 1]);
+% set(gcf, 'color', [1 1 1]);
 % 
 % subplot(2,2,3)
 % plot(EN,Output(MaxInd,:),'r-');
@@ -286,6 +292,7 @@ num2str(MaxInd)
 % title(['Best Fit: Spectrum ' num2str(MinInd) ': ChiSq ' num2str(MinRChiSq)]);
 % xlim([EN(Windows(1)-PlotBuffer) EN(Windows(4)+PlotBuffer)]);
 % ax = gca; ax.YAxis.Exponent = 0;
-% 
-Linescan_SubtractPower=single(Output);
-WriteTif(Linescan_SubtractPower,String);
+
+
+Linescan_SubtractLCPL=single(Output);
+WriteTif(Linescan_SubtractLCPL,String);
